8b49ab563afd9bdc1287a4fbcdcfa6b6d2b6a802
Phi <dterberg98@gmail.com>
2025-01-05 19:22:45 +0100

    decode

--- sync.py
+++ sync.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 # path is relative to music folder
 def get_music_file(music_folder: str, path: str, url: str):
-    res = requests.request("get", f"{url}/music", data=path)
+    res = requests.request("get", f"{url}/music", data=path.decode())
     with open(f"{music_folder}{path}", "bw") as file:
         file.write(res.content)
 
